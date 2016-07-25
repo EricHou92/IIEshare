4b48665fc6d4dcd50b128bf19d562e359daba763
ciciyaya <1551723566@qq.com>
2016-07-25 16:10:23 +0800

    增加一个发送接收主界面

---
+++ app/src/main/java/com/hou/iieshare/ui/main/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hou.iieshare.ui.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.hou.iieshare.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
